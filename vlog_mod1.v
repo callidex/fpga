4c8f00bdc90f71d5c570e3e63b19020bf5af6747
callidex <callidex@users.noreply.github.com>
2017-09-06 14:21:11 +1000

    add mini counter module

--- vlog_mod1.v
+++ vlog_mod1.v
@@ -19,17 +19,29 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module vlog_mod1(
-    input A,
-    input B,
-    input SEL,
-    output req Q
-    );
-	 
-	 always @(A or B or SEL)
-	 begin
+   input A,
+   input B,
+   input SEL,
+   output req Q
+   );
+		 
+	always @(A or B or SEL)
+	begin
 		if(SEL)
 			Q = A;
 		else
 			Q = B;
-	 end
+	end
 endmodule
+
+module counter(  
+	input Clock, 
+	output reg [3:0] Q 
+	);
+
+always @(posedge Clock)
+	begin
+		Q <= Q + 1;
+	end
+endmodule;
+
